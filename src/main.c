01c966d2c707c2685684ca5a32351e6a99a6782b
TangCheng <tangcheng2005@gmail.com>
2014-11-01 16:16:53 +0800

    完成接收文件时，解析数据头尾的boundary信息，以及创建upgrade-start文件。

--- src/main.c
+++ src/main.c
@@ -1,59 +1,113 @@
+#define _GNU_SOURCE
+#include <stdio.h>
 #include "fcgi_stdio.h"
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <memory.h>
+#include "multipart_parser.h"
 
-
-void main(void)
+int on_part_data(multipart_parser *parser, const char *data, size_t length)
 {
+#if 0
+    printf("Content-type: text/html\r\n\r\n<html><body>got data</body></html>\r\n");
+    fflush(stdout);
+#endif
+    FCGI_FILE * fileOut = FCGI_fopen("/var/cache/firmware-hi3518.bin", "a");
+    if (fileOut)
+    {
+        FCGI_fwrite((void *)data, 1, length, fileOut);
+        FCGI_fclose(fileOut);
+    }
+    
+    return 0;
+}
 
-    int count = 0;
-    while(FCGI_Accept() >= 0) {
+int on_part_data_end(multipart_parser *parser)
+{
+    system("mv /var/cache/firmware-hi3518.bin /var/cache/firmware/");
+    FCGI_FILE *fileOut = FCGI_fopen("/var/cache/firmware/upgrade-start", "w");
+    if (fileOut)
+    {
+        FCGI_fclose(fileOut);
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    while(FCGI_Accept() >= 0)
+    {
         char *contentLength = getenv("CONTENT_LENGTH");
-        int len;
-
-        if (contentLength != NULL) {
-            len = strtol(contentLength, NULL, 10);
+        char *org_boundary = getenv("CONTENT_TYPE");
+#if 0
+        if (org_boundary)
+        {
+            printf("Content-type: text/html\r\n\r\n<html><body>%s</body></html>\r\n", org_boundary);
         }
-        else {
-            len = 0;
+        else
+        {
+            printf("Content-type: text/html\r\n\r\n<html><body>not found boundary</body></html>\r\n");
         }
-        printf("Content-type: text/html\r\n"
-               "\r\n"
-               "<title>FastCGI Hello!</title>"
-               "<h1>FastCGI Hello!</h1>"
-               "Request number %d running on host <i>%s</i>\n",
-                ++count, getenv("SERVER_NAME"));
-
-        printf("<br />CONTENT_LENGTH = %d <br />\r\n", len);
-        printf("<form enctype='multipart/form-data' method='post' action='?'><input type='text' name='text1' /><input type='file' name='file1'/><input type='submit' /></form>");
-        printf("<hr />");
-
         fflush(stdout);
+#endif        
+        org_boundary = strstr(org_boundary, "boundary=");
+        if (org_boundary)
+        {
+            char *boundary = NULL;
+            int len;
 
-        FCGI_FILE * fileOut = FCGI_fopen("/tmp/fcgi.out", "w");
-        if (fileOut) {
-            int done = 0;
-            while(done < len) {
-                char buffer[1024];
-                int i;
-                int packetRead;
-
-                packetRead = FCGI_fread(buffer, 1, sizeof(buffer), stdin);
-                if (packetRead < 0) {
-                    break;
+            asprintf(&boundary, "--%s", org_boundary + strlen("boundary="));
+            if (boundary)
+            {
+                if (contentLength != NULL)
+                {
+                    len = strtol(contentLength, NULL, 10);
                 }
-                if (packetRead > 0) {
-                    FCGI_fwrite(buffer, 1, packetRead, fileOut);
-                    done += packetRead;
+                else
+                {
+                    len = 0;
                 }
 
+                int done = 0;
+                char *buffer = malloc(len);
+                if (buffer)
+                {
+                    while(done < len)
+                    {
+                        int packetRead;
 
+                        packetRead = FCGI_fread(buffer + done, 1, len - done, stdin);
+                        if (packetRead < 0)
+                        {
+                            break;
+                        }
+                        if (packetRead > 0)
+                        {
+                            done += packetRead;
+                        }
+                    }
+                    multipart_parser_settings callbacks;
+                    memset(&callbacks, 0, sizeof(multipart_parser_settings));
+                    callbacks.on_part_data = on_part_data;
+                    callbacks.on_part_data_end = on_part_data_end;
+                    multipart_parser* parser = multipart_parser_init(boundary, &callbacks);
+                    multipart_parser_execute(parser, buffer, len);
+                    multipart_parser_free(parser);
+#if 0
+                    printf("Content-type: text/html\r\n\r\n<html><body>%s<br>%s</body></html>\r\n", boundary, buffer);
+                    fflush(stdout);
+#endif
+                    free(buffer);
+                }
+                free(boundary);
             }
-            FCGI_fclose(fileOut);
         }
-
+        
         FCGI_Finish();
     }
+
+    return 0;
 }
